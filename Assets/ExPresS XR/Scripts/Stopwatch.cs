b0010673afbd48851be90a18f5a6b57fdc0f374e
eisclimber <dreililu@gmx.de>
2023-03-01 01:10:06 +0100

    Added IK Setup Dialog

--- Assets/ExPresS XR/Scripts/Stopwatch.cs
+++ Assets/ExPresS XR/Scripts/Stopwatch.cs
@@ -2,56 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stopwatch : MonoBehaviour
+namespace ExPresSXR.Timing
 {
-    const float INACTIVE_STOP_TIME = -1.0f;
-
-    [Tooltip("Time when the stopwatch was started or INACTIVE_STOP_TIME if not started.")]
-    private float _startTime = INACTIVE_STOP_TIME;
-    public float startTime { get; private set; }
-
-    [Tooltip("How long the stopwatch is currently running or INACTIVE_STOP_TIME if not started.")]
-    public float currentStopTime
+    public class Stopwatch : MonoBehaviour
     {
-        get 
+        const float INACTIVE_STOP_TIME = -1.0f;
+
+        [Tooltip("Time when the stopwatch was started or INACTIVE_STOP_TIME if not started.")]
+        private float _startTime = INACTIVE_STOP_TIME;
+        public float startTime { get; private set; }
+
+        [Tooltip("How long the stopwatch is currently running or INACTIVE_STOP_TIME if not started.")]
+        public float currentStopTime
         {
-            return _startTime == INACTIVE_STOP_TIME ? INACTIVE_STOP_TIME : (Time.time - _startTime);
+            get 
+            {
+                return _startTime == INACTIVE_STOP_TIME ? INACTIVE_STOP_TIME : (Time.time - _startTime);
+            }
         }
-    }
 
-    [Tooltip("If true, will start the timer OnAwake.")]
-    public bool autoStart;
-
-
-    private bool _running;
-    public bool running { get; private set; }
+        [Tooltip("If true, will start the timer OnAwake.")]
+        public bool autoStart;
 
 
-    private void Awake() {
-        if (autoStart)
+        private bool _running;
+        public bool running { get; private set; }
+
+
+        private void Awake() {
+            if (autoStart)
+            {
+                StartTimeMeasurement();
+            }
+        }
+
+        // Starts the stopwatch
+        public void StartTimeMeasurement()
         {
-            StartTimeMeasurement();
+            _startTime = Time.time;
+            running = true;
         }
-    }
 
-    // Starts the stopwatch
-    public void StartTimeMeasurement()
-    {
-        _startTime = Time.time;
-        running = true;
-    }
-
-    // Stops and resets the stopwatch, returns the final time measurement
-    public float StopTimeMeasurement(bool _restart = false)
-    {
-        // Save end time
-        float endTime = currentStopTime;
-        
-        // Halt stopwatch or restart
-        running = _restart;
-        _startTime = _restart ? Time.time : INACTIVE_STOP_TIME;
-        
-        // Return the previously saved time
-        return endTime;
+        // Stops and resets the stopwatch, returns the final time measurement
+        public float StopTimeMeasurement(bool _restart = false)
+        {
+            // Save end time
+            float endTime = currentStopTime;
+            
+            // Halt stopwatch or restart
+            running = _restart;
+            _startTime = _restart ? Time.time : INACTIVE_STOP_TIME;
+            
+            // Return the previously saved time
+            return endTime;
+        }
     }
 }
--- Assets/ExPresS XR/Scripts/Timer.cs
+++ Assets/ExPresS XR/Scripts/Timer.cs
@@ -3,107 +3,110 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Timer : MonoBehaviour
+namespace ExPresSXR.Timing
 {
-    const float TIMER_INACTIVE_WAIT_TIME = -1.0f;
-    const float DEFAULT_WAIT_TIME = 1.0f;
-    
-
-    [Tooltip("How long the timer takes to timeout.")]
-    [SerializeField]
-    private float _waitTime = DEFAULT_WAIT_TIME;
-    public float waitTime
+    public class Timer : MonoBehaviour
     {
-        get => _waitTime;
-        set
+        const float TIMER_INACTIVE_WAIT_TIME = -1.0f;
+        const float DEFAULT_WAIT_TIME = 1.0f;
+        
+
+        [Tooltip("How long the timer takes to timeout.")]
+        [SerializeField]
+        private float _waitTime = DEFAULT_WAIT_TIME;
+        public float waitTime
         {
-            if (value <= 0.0f)
+            get => _waitTime;
+            set
             {
-                Debug.Log("Wait time must be greater than 0 seconds.");
-            }
-            else
-            {
-                _waitTime = value;
-            }
-        }
-    }
-
-    [Tooltip("If true, will start the timer OnAwake.")]
-    public bool autoStart;
-
-    [Tooltip("If false, the timer will restart after timeout.")]
-    public bool oneShot;
-
-
-    [Tooltip("Emitted when the timer is started.")]
-    public UnityEvent OnStarted;
-
-    [Tooltip("Emitted when the timer times out.")]
-    public UnityEvent OnTimeout;
-
-
-    private bool timerActive;
-    private bool timerPaused;
-    private float remainingTime;
-
-
-    private void Awake() {
-        if (autoStart)
-        {
-            StartTimer();
-        }
-    }
-    
-    private void FixedUpdate() {
-        if (timerActive && !timerPaused)
-        {
-            remainingTime -= Time.fixedDeltaTime;
-
-            if (remainingTime <= 0.0f)
-            {
-                HandleTimeout();
+                if (value <= 0.0f)
+                {
+                    Debug.Log("Wait time must be greater than 0 seconds.");
+                }
+                else
+                {
+                    _waitTime = value;
+                }
             }
         }
-    }
 
-    private void HandleTimeout()
-    {
-        StopTimer();
-        OnTimeout.Invoke();
+        [Tooltip("If true, will start the timer OnAwake.")]
+        public bool autoStart;
 
-        if (!oneShot)
-        {
-            StartTimer();
+        [Tooltip("If false, the timer will restart after timeout.")]
+        public bool oneShot;
+
+
+        [Tooltip("Emitted when the timer is started.")]
+        public UnityEvent OnStarted;
+
+        [Tooltip("Emitted when the timer times out.")]
+        public UnityEvent OnTimeout;
+
+
+        private bool timerActive;
+        private bool timerPaused;
+        private float remainingTime;
+
+
+        private void Awake() {
+            if (autoStart)
+            {
+                StartTimer();
+            }
         }
-    }
+        
+        private void FixedUpdate() {
+            if (timerActive && !timerPaused)
+            {
+                remainingTime -= Time.fixedDeltaTime;
 
-    // (Re-)starts the timer with duration, setting waitTime in the process.
-    // If duration is <= 0.0f the value of waitTime is used.
-    public void StartTimer(float duration = -1.0f)
-    {
-        waitTime = duration > 0.0f? duration : waitTime;
-        remainingTime = waitTime;
-        timerActive = true;
-        OnStarted.Invoke();
-    }
+                if (remainingTime <= 0.0f)
+                {
+                    HandleTimeout();
+                }
+            }
+        }
 
-    // Pauses the timer, maintaining it's current waitTime
-    public void PauseTimer(bool paused)
-    {
-        timerPaused = paused;
-    }
+        private void HandleTimeout()
+        {
+            StopTimer();
+            OnTimeout.Invoke();
 
-    // Stops and resets the timer whilst not emitting the timeout event
-    public void StopTimer()
-    {
-        remainingTime = TIMER_INACTIVE_WAIT_TIME;
-        timerActive = false;
-    }
+            if (!oneShot)
+            {
+                StartTimer();
+            }
+        }
 
-    // Returns the remaining time of the timer.
-    // If the timer is not running the value will be the value of TIMER_INACTIVE_WAIT_TIME.
-    public float GetRemainingTime()
-    {
-        return remainingTime;
+        // (Re-)starts the timer with duration, setting waitTime in the process.
+        // If duration is <= 0.0f the value of waitTime is used.
+        public void StartTimer(float duration = -1.0f)
+        {
+            waitTime = duration > 0.0f? duration : waitTime;
+            remainingTime = waitTime;
+            timerActive = true;
+            OnStarted.Invoke();
+        }
+
+        // Pauses the timer, maintaining it's current waitTime
+        public void PauseTimer(bool paused)
+        {
+            timerPaused = paused;
+        }
+
+        // Stops and resets the timer whilst not emitting the timeout event
+        public void StopTimer()
+        {
+            remainingTime = TIMER_INACTIVE_WAIT_TIME;
+            timerActive = false;
+        }
+
+        // Returns the remaining time of the timer.
+        // If the timer is not running the value will be the value of TIMER_INACTIVE_WAIT_TIME.
+        public float GetRemainingTime()
+        {
+            return remainingTime;
+        }
     }
 }
