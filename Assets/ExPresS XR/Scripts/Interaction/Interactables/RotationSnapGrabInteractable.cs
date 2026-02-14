fffa9f2335b9b3d8f0f7e9b3990ff793b88609f1
eisclimber <49446532+eisclimber@users.noreply.github.com>
2026-02-14 14:00:45 +0100

    Perform snapping in socket interactor

--- Assets/ExPresS XR/Scripts/Interaction/Interactables/RotationSnapGrabInteractable.cs
+++ Assets/ExPresS XR/Scripts/Interaction/Interactables/RotationSnapGrabInteractable.cs
@@ -9,43 +9,52 @@ namespace ExPresSXR.Interaction.Interactables
     {
         public UnityEvent<int> OnRotationSnapped;
 
+        [SerializeField]
+        private bool test = false;
+
         protected override void OnSelectEntering(SelectEnterEventArgs args)
         {
-            if (args.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor)
-            {
-                MatchAttachRotation(args.interactorObject);
-            }
+            // if (args.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor)
+            // {
+            //     MatchAttachRotation(args.interactorObject);
+            // }
 
             base.OnSelectEntering(args);
         }
 
-        private void MatchAttachRotation(UnityEngine.XR.Interaction.Toolkit.Interactors.IXRSelectInteractor interactor)
-        {
-            // Create attach transform if missing
-            if (attachTransform == null)
-            {
-                GameObject attachGo = new("Attach Transform (Generated)");
-                attachGo.transform.SetParent(transform);
-                attachGo.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
-                attachTransform = attachGo.transform;
-            }
+        // private void MatchAttachRotation(UnityEngine.XR.Interaction.Toolkit.Interactors.IXRSelectInteractor interactor)
+        // {
+        //     // Create attach transform if missing
+        //     if (attachTransform == null)
+        //     {
+        //         GameObject attachGo = new("Attach Transform (Generated)");
+        //         attachGo.transform.SetParent(transform);
+        //         attachGo.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        //         attachTransform = attachGo.transform;
+        //     }
 
-            // Transform own rotation to the sockets attach local space (ignoring rotation of our attach!)
-            Transform socketAttach = interactor.GetAttachTransform(this);
-            Quaternion worldToSocketLocal = Quaternion.Inverse(socketAttach.rotation);
-            Quaternion ownInSocketLocal = transform.rotation * worldToSocketLocal;
+        //     // Transform own rotation to the sockets attach local space (ignoring rotation of our attach!)
+        //     Transform socketAttach = interactor.GetAttachTransform(this);
+        //     Transform ownAttach = GetAttachTransform(interactor);
+        //     if (test) Debug.Log("Socket Attach Rotation: " + socketAttach.rotation.eulerAngles + " vs ownAttach.rotation.eulerAngles: " + ownAttach.rotation.eulerAngles);
+        //     Quaternion worldToSocketLocal = Quaternion.Inverse(socketAttach.rotation);
+        //     Quaternion ownInSocketLocal = ownAttach.rotation * worldToSocketLocal;
+        //     if (test) Debug.Log("Own in Socket Local Rotation: " + ownInSocketLocal.eulerAngles);
 
-            // Snap own rotation to the xz-plane and 90 degree increments of y-rotation in the sockets space
-            float eulerY = ownInSocketLocal.eulerAngles.y;
-            int rotationSnaps = Mathf.RoundToInt(eulerY / 90.0f);
-            Vector3 eulerSnapped = new(0.0f, rotationSnaps * 90.0f, 0.0f);
-            Quaternion ownInSocketLocalSnapped = Quaternion.Euler(eulerSnapped);
-            // Convert snapped value to world space
-            Quaternion snappedInWorldSpace = ownInSocketLocalSnapped * socketAttach.rotation;
-            // Finally, we want to compensate the rotation -> use inverse 
-            attachTransform.localRotation = Quaternion.Inverse(snappedInWorldSpace);
-            // Emit signal to trigger rotation data
-            OnRotationSnapped.Invoke(rotationSnaps);
-        }
+        //     // Snap own rotation to the xz-plane and 90 degree increments of y-rotation in the sockets space
+        //     float eulerY = ownInSocketLocal.eulerAngles.y;
+        //     if (test) Debug.Log("Euler Y: " + eulerY);
+        //     int rotationSnaps = Mathf.RoundToInt(eulerY / 90.0f);
+        //     if (test) Debug.Log("Rotation Snaps: " + rotationSnaps);
+        //     Vector3 eulerSnapped = new(0.0f, rotationSnaps * 90.0f, 0.0f);
+        //     if (test) Debug.Log("Euler Snapped: " + eulerSnapped);
+        //     Quaternion ownInSocketLocalSnapped = Quaternion.Euler(eulerSnapped);
+        //     // Convert snapped value to world space
+        //     Quaternion snappedInWorldSpace = ownInSocketLocalSnapped * socketAttach.rotation;
+        //     // Finally, we want to compensate the rotation -> use inverse 
+        //     attachTransform.localRotation = Quaternion.Inverse(snappedInWorldSpace);
+        //     // Emit signal to trigger rotation data
+        //     OnRotationSnapped.Invoke(rotationSnaps);
+        // }
     }
 }
--- Assets/ExPresS XR/Scripts/Minigames/Tile Game/TileSubmitSocket.cs
+++ Assets/ExPresS XR/Scripts/Minigames/Tile Game/TileSubmitSocket.cs
@@ -4,6 +4,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using ExPresSXR.Interaction.Interactors;
 using ExPresSXR.Interaction.Interactables;
+using ExPresSXR.Misc;
 
 namespace ExPresSXR.Minigames.TileGame
 {
@@ -25,17 +26,42 @@ namespace ExPresSXR.Minigames.TileGame
         [Tooltip("Disable this socket and the interactable if possible on submission.")]
         private bool _disableOnSelect = true;
 
+        [SerializeField]
+        private int _snapAngle = 90;
+
+        [SerializeField]
+        private bool _requireFrontSideUp = true;
+
         private XRBaseInteractable _interactable;
+
+        private int NumSteps
+        {
+            get => Mathf.RoundToInt(360f / _snapAngle);
+        }
 
         /// <summary>
         /// Emitted once an object has been submitted.
         /// </summary>
         public UnityEvent OnSubmitted;
+        public UnityEvent<int> OnRotationSnapped;
         public UnityEvent<BoardSubmitContext> OnTileSubmitted;
 
         protected override void OnEnable()
         {
             base.OnEnable();
+
+            if (360 % _snapAngle != 0)
+            {
+                Debug.LogWarning("Snap angle should be a divisor of 360 to work properly.", this);
+            }
+
+            if (attachTransform == null)
+            {
+                // Create a new attach transform if not set as it is required to properly snap the rotation
+                GameObject attachGo = new("Attach Transform (Generated)");
+                attachGo.transform.SetParent(transform, false);
+                attachTransform = attachGo.transform;
+            }
 
             SetHighlighterVisible(ShowHighlighter && startingSelectedInteractable == null);
 
@@ -57,6 +83,50 @@ namespace ExPresSXR.Minigames.TileGame
             {
                 _interactable.gameObject.SetActive(false);
             }
+        }
+
+        protected override void OnSelectEntering(SelectEnterEventArgs args)
+        {
+            base.OnSelectEntering(args);
+
+            if (args.interactableObject is XRGrabInteractable grab)
+            {
+                SnapRotation(grab);
+            }
+        }
+
+
+        private void SnapRotation(XRGrabInteractable interactable)
+        {
+            if (attachTransform == null)
+            {
+                Debug.LogError("No attach transform found. This is required for snapping to work.", this);
+                return;
+            }
+
+            Transform ownAttach = attachTransform;
+            Transform interactableAttach = interactable.GetAttachTransform(this);
+
+            // Convert to socket local space
+            Quaternion local = Quaternion.Inverse(ownAttach.rotation) * interactableAttach.rotation;
+
+            // Get angle around socket up axis
+            Vector3 forward = local * Vector3.forward;
+            float rawAngle = Mathf.Atan2(forward.x, forward.z) * Mathf.Rad2Deg;
+            int roundedAngle = (int)Mathf.Round(rawAngle / _snapAngle);
+            int step = RuntimeUtils.PosMod(roundedAngle, NumSteps);
+
+            // Snap to increments
+            float steppedAngle = step * _snapAngle;
+
+            // Build snapped local rotation
+            Quaternion snappedLocal = Quaternion.AngleAxis(steppedAngle, Vector3.up);
+            
+            // Respecting the original rotation, adjust the socketAttach
+            ownAttach.rotation = transform.rotation * snappedLocal;
+
+            // Emit snap signal
+            OnRotationSnapped.Invoke(step);
         }
 
         /// <summary>
@@ -88,13 +158,44 @@ namespace ExPresSXR.Minigames.TileGame
             }
         }
 
+        
+
+        
+
         public override bool CanHover(IXRHoverInteractable interactable)
-            => base.CanHover(interactable) && IsObjectMatch(interactable);
+            => base.CanHover(interactable) && IsTileVisualsMatch(interactable) && (!_requireFrontSideUp || IsFrontSideUp(interactable));
 
         public override bool CanSelect(IXRSelectInteractable interactable)
-            => base.CanSelect(interactable) && IsObjectMatch(interactable);
+            => base.CanSelect(interactable) && IsTileVisualsMatch(interactable) && (!_requireFrontSideUp || IsFrontSideUp(interactable));
 
-        private bool IsObjectMatch(IXRInteractable interactable) => interactable.transform.TryGetComponent(out TileVisuals _);
+        private bool IsTileVisualsMatch(IXRInteractable interactable) => interactable.transform.TryGetComponent(out TileVisuals _);
+
+        private bool IsFrontSideUp(IXRInteractable interactable)
+        {
+            // Check if the up vector align
+            Transform ownAttach = GetAttachTransform(interactable);
+            Transform interactableAttach = interactable.GetAttachTransform(this);
+            float dot = Vector3.Dot(ownAttach.up, interactableAttach.up);
+            return dot > 0.0f; // Adjust the threshold as needed
+        }
+
+        protected virtual void OnDrawGizmos()
+        {
+            // Change to local space
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(Vector3.zero, Vector3.up * 0.05f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(Vector3.zero, Vector3.forward * 0.05f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(Vector3.zero, Vector3.right * 0.05f);
+            // Draw snap points
+            Gizmos.color = Color.pink;
+            GizmoUtils.DrawLabel("0", Vector3.forward * 0.08f, transform);
+            GizmoUtils.DrawLabel("1", Vector3.right * 0.08f, transform);
+            GizmoUtils.DrawLabel("2", -Vector3.forward * 0.08f, transform);
+            GizmoUtils.DrawLabel("3", -Vector3.right * 0.08f, transform);
+        }
 
         public class BoardSubmitContext
         {
